21ac475638fb99769cc3e760007c539df3b31ce9
Humam Taibeh <130459546+Humam-Taibeh@users.noreply.github.com>
2026-01-05 22:42:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    chunkSizeWarningLimit: 2000, // تم رفع الحد لـ 2000kb لتجنب التنبيه في Vercel
-  }
+    chunkSizeWarningLimit: 2000, // رفع الحد لـ 2000 كيلوبايت
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // تجزئة المكتبات الكبيرة لملفات منفصلة لتقليل حجم الـ Chunk الواحد
+          if (id.includes('node_modules')) {
+            if (id.includes('firebase')) return 'vendor-firebase';
+            if (id.includes('framer-motion')) return 'vendor-framer';
+            if (id.includes('lucide-react')) return 'vendor-icons';
+            return 'vendor'; // باقي المكتبات
+          }
+        },
+      },
+    },
+  },
 })
