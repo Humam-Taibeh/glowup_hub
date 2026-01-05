94d79a17f47b06111ca6f044eee8017d78e19a3e
Humam Taibeh <130459546+Humam-Taibeh@users.noreply.github.com>
2026-01-05 22:38:17 +0300

    Update CSS import comment for clarity

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // هذا السطر هو اللي بجيب الألوان
+import './index.css' // ضروري جداً
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
