09befd28482c1f71d1ef6c7f950c0d6f3c4fc0fb
Humam Taibeh <130459546+Humam-Taibeh@users.noreply.github.com>
2026-01-05 17:18:22 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSy...",
+  authDomain: "glowup-nexus.firebaseapp.com",
+  projectId: "glowup-nexus",
+  storageBucket: "glowup-nexus.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
+};
