3a09ae1d1b1ea8a580f329ea97b48c84bc8fe206
Humam Taibeh <130459546+Humam-Taibeh@users.noreply.github.com>
2026-01-05 22:44:51 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 2000, // رفع الحد لـ 2000 كيلوبايت
+    // رفع الحد لـ 3000 كيلوبايت لضمان عدم ظهور التحذير في Vercel
+    chunkSizeWarningLimit: 3000, 
     rollupOptions: {
       output: {
+        // توزيع المكتبات لملفات منفصلة (Code Splitting) لزيادة الأداء
         manualChunks(id) {
-          // تجزئة المكتبات الكبيرة لملفات منفصلة لتقليل حجم الـ Chunk الواحد
           if (id.includes('node_modules')) {
             if (id.includes('firebase')) return 'vendor-firebase';
-            if (id.includes('framer-motion')) return 'vendor-framer';
+            if (id.includes('framer-motion')) return 'vendor-motion';
             if (id.includes('lucide-react')) return 'vendor-icons';
-            return 'vendor'; // باقي المكتبات
+            return 'vendor';
           }
         },
       },
